1766fef7a40460df980f79f27ea8067b8f5201a1
Amilil <amililuing86@gmail.com>
2025-10-13 20:30:04 +0700

    Proyek 2 - Manage user information

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+
+import '../main.dart'; // Akses MyApp.darkModeNotifier
 import 'about_screen.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
 
   @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  @override
   Widget build(BuildContext context) {
+    bool _darkMode = MyApp.darkModeNotifier.value;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Settings"),
@@ -13,6 +22,18 @@ class SettingsScreen extends StatelessWidget {
       ),
       body: ListView(
         children: [
+          SwitchListTile(
+            title: const Text("Dark Mode"),
+            secondary: const Icon(Icons.dark_mode, color: Colors.blue),
+            value: _darkMode,
+            onChanged: (value) {
+              setState(() {
+                _darkMode = value;
+              });
+              // 🔥 Ubah dark mode global
+              MyApp.darkModeNotifier.value = value;
+            },
+          ),
           ListTile(
             leading: const Icon(Icons.info, color: Colors.blue),
             title: const Text("About"),
